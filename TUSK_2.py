73275e26a241626431575257959f199ad426f3c4
GregorySmok <g.smokovdin@yandex.ru>
2023-10-15 15:04:23 +0300

    rename

--- TUSK_2.py
+++
@@ -1,7 +0,0 @@
-print('w x y z')
-for x in (0,1):
-    for y in (0, 1):
-        for w in (0, 1):
-            for z in (0, 1):
-                if (not(x) or not(y)) and (x != z) and w:
-                    print(w, x, y, z)
