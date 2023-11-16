8eafd4d205178ab6c8020a56b14266a39a4b48e4
GregorySmok <g.smokovdin@yandex.ru>
2023-11-16 17:47:50 +0300

    upd

--- TASK_19_20_21.py
+++ TASK_19_20_21.py
@@ -1,16 +1,14 @@
-def func(f, s, h, pob):
-    if f + s >= 75:
+def func(s, h, pob):
+    if s >= 39:
         return h in pob
     if h > max(pob):
         return 0
-    m = [func(f, s+1, h+1, pob), func(f+1, s, h+1, pob),
-         func(f+s, s, h+1, pob), func(f, s + f, h+1, pob)]
+    m = [func(s+1, h+1, pob), func(s+2, h+1, pob), func(s*2, h+1, pob)]
     if h % 2 != max(pob) % 2:
         return any(m)
     else:
         return all(m)
-
-
-for s in range(1, 47):
-    if func(7, s, 0, [2]) != 1 and func(7, s, 0, [2, 4]) == 1:
+    
+for s in range(1, 39):
+    if func(s, 0, [2]) != 1 and func(s, 0, [2, 4]) == 1:
         print(s)
--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-count = 0
-for n in range(1000, 10000):
-    if str(n)[0] in '24680':
-        r = int(str(n)[0]) + int(str(n)[2]) + \
-            abs(int(str(n)[1]) - int(str(n)[3]))
-    else:
-        d = int(''.join(sorted(str(n))))
-        r = bin(d)[2:].count('1')
-    if r > 20:
-        count += 1
-print(count)
+a = 7.5
+print(a.__class__ == float)
