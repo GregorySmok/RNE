9db64de6ee727ba1170e1375b77e583146d624d7
GregorySmok <g.smokovdin@yandex.ru>
2024-03-03 14:08:32 +0300

    upd

--- TASK_26.py
+++ TASK_26.py
@@ -1,17 +1,25 @@
-with open('DATA/26 (1).txt') as f:
+'''with open('DATA/26 (2).txt') as f:
+    data = [int(i) for i in f]
+    rog = sorted(data[:117], reverse=True)
+    pr = sorted(data[117:], reverse=True)
+s = 0
+for el in rog:
+    for pres in pr:
+        if pres <= el:
+            s += pres
+            pr.remove(pres)
+            break
+print(s)'''
+
+with open('DATA/26 (3).txt') as f:
     data = [[int(i.split()[0]), int(i.split()[1])] for i in f]
     data.sort(key=lambda x: (x[0], x[1]))
-mc = 0
-lt = 0
+print(data[:30])
+count = 0
+ln = 0
 for i in range(len(data) - 1):
-    cc = 1
-    start = data[i]
-    last = data[i]
-    for j in range(i + 1, len(data)):
-        if last[1] <= data[j][0]:
-            last = data[j]
-            cc += 1
-    if mc <= cc:
-        mc = cc
-        lt = start[0]
-print(mc, lt)
+    if (data[i + 1][0] - 1) - (data[i][1] + 1) > 0:
+        print(data[i], data[i + 1])
+        count += 1
+        ln += data[i + 1][0] - data[i][1] - 1
+print(count, ln + 1)
